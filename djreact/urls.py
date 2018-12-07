d021226d5c21877b1a3840d5d20733ae97952c53
Sollami Florian <florian.sollami@student.vinci.be>
2018-12-07 08:57:57 +0100

    Déploiement 3

--- djreact/urls.py
+++ djreact/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('music.api.urls'))
+    path('api/', include('music.api.urls')),
+    re_path('.*', TemplateView.as_view(template_name="index.html"))
 ]
